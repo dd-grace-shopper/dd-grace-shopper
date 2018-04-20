6cadc2c405119910d1bcba649e4f98a6473fd040
Yoni Slotwinere <yonislots@gmail.com>
2018-04-20 16:16:00 -0400

    filter products button working with sidebar

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -3,18 +3,11 @@ import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui
 import { connect } from 'react-redux';
 
 export function SidebarLeft (props) {
-  // state = { visible: false }
-
-  // toggleVisibility = () => this.setState({ visible: !this.state.visible })
-
-  // const { visible } = this.state
   let visible = props.visible
 
   return (
     <div>
-
-      {/*<Sidebar as={Menu} animation='push' width='thin' visible={visible} icon='labeled' vertical inverted>*/}
-      <Sidebar visible={visible}>
+      <Sidebar as={Menu} className="ui vertical inverted" animation="push" visible={visible}>
         <Menu.Item name='category'>Category</Menu.Item>
         <Menu.Item name='size'>Size</Menu.Item>
         <Menu.Item name='country'>Country</Menu.Item>
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -29,7 +29,6 @@ const Navbar = ({ handleClick, isLoggedIn }) => (
         </div>
       )}
     </nav>
-    <hr />
   </div>
 );
 
