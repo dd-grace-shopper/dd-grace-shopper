9c05ac5d8a757e69b5a9b4d95c4be79041444a52
Yoni Slotwinere <yonislots@gmail.com>
2018-04-20 17:13:32 -0400

    added form inputs to sidebar filter

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react'
+import { Sidebar, Segment, Button, Menu, Image, Icon, Header, Form, Checkbox } from 'semantic-ui-react'
 import { connect } from 'react-redux';
 
 export function SidebarLeft (props) {
@@ -8,9 +8,25 @@ export function SidebarLeft (props) {
   return (
     <div>
       <Sidebar as={Menu} className="ui vertical inverted" animation="push" visible={visible}>
-        <Menu.Item name='category'>Category</Menu.Item>
-        <Menu.Item name='size'>Size</Menu.Item>
-        <Menu.Item name='country'>Country</Menu.Item>
+        <Form.Group inline>
+          <label>Category</label>
+            <Form.Field label='Red Wine' control='input' type='checkbox' />
+            <Form.Field label='White Wine' control='input' type='checkbox' />
+            <Form.Field label='Whiskey' control='input' type='checkbox' />
+        </Form.Group>
+        <Form.Group inline>
+          <label>Size</label>
+            <Form.Field label='small' control='input' type='checkbox' />
+            <Form.Field label='medium' control='input' type='checkbox' />
+            <Form.Field label='large' control='input' type='checkbox' />
+        </Form.Group>
+        <Form.Group inline>
+          <label>Price</label>
+            <Form.Field label='$' control='input' type='checkbox' />
+            <Form.Field label='$$' control='input' type='checkbox' />
+            <Form.Field label='$$$' control='input' type='checkbox' />
+            <Form.Field label='$$$$' control='input' type='checkbox' />
+        </Form.Group>
       </Sidebar>
 
     </div>
