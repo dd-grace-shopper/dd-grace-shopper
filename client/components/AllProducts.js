2c459bc16cec1b48460c21223465cb884ed3112f
Ben Ellentuck <ellentuckben@gmail.com>
2018-04-19 20:47:40 -0400

    implemented cart crud, everything is working, just need to add some front-end pieces to the mix

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,38 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import AddToCart from './AddToCart';
 import { Link } from 'react-router-dom';
 
-export default class AllProducts extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidMount() {
-    this.props.getAllProducts ? this.props.getAllProducts() : null;
-  }
-
-  render() {
-    const { products, productsById, cookieId } = this.props;
-
-    return (
-      <div>
-        <h1>All Products</h1>
-        {products &&
-          products.map(id => {
-            const product = productsById[id];
-            return (
-              <div key={id}>
-                <div>
-                  <Link to={`products/${id}`}>
-                    <h2>{product.name}</h2>
-                  </Link>
-                  <img className="product-img" src={product.imageUrl} />
-                  <AddToCart productId={id} />
-                </div>
+const AllProducts = props => {
+  const { products, productsById } = props;
+  return (
+    <div>
+      <h1>All Products</h1>
+      {products &&
+        products.map(id => {
+          const product = productsById[id];
+          return (
+            <div key={id}>
+              <div>
+                <Link to={`products/${id}`}>
+                  <h2>{product.name}</h2>
+                </Link>
+                <img className="product-img" src={product.imageUrl} />
+                <AddToCart productId={id} />
               </div>
-            );
-          })}
-      </div>
-    );
-  }
+            </div>
+          );
+        })}
+    </div>
+  );
 }
+
+export default AllProducts;
--- client/store/cart.js
+++ client/store/cart.js
@@ -1,7 +1,19 @@
 import axios from 'axios';
 
+// types
+const CREATE_CART = 'CREATE_CART';
 const ADD_TO_CART = 'ADD_TO_CART';
+const UPDATE_ITEM_IN_CART = 'UPDATE_ITEM_IN_CART';
+const DELETE_FROM_CART = 'DELETE_FROM_CART';
 
+// action creators
+export function createCart(newCart) {
+  const action = {
+    type: CREATE_CART,
+    newCart
+  }
+  return action;
+}
 
 export function addToCart(order) {
   const action = {
@@ -11,6 +23,38 @@ export function addToCart(order) {
   return action;
 }
 
+export function updateItemInCart(item) {
+  const action = {
+    type: UPDATE_ITEM_IN_CART,
+    item
+  }
+  return action;
+}
+
+export function deleteFromCart(productId) {
+  const action = {
+    type: DELETE_FROM_CART,
+    productId
+  }
+  return action;
+}
+
+
+// thunk creators
+export const fetchCart = () => dispatch => {
+  return axios
+  .get('/api/active-orders')
+  .then(res => res.data)
+  .then(cartFromDb => {
+    const newCart = cartFromDb.reduce((newCartObj, item) => {
+      const { productId, quantity } = item;
+      newCartObj[productId] = { productId, quantity };
+      return newCartObj;
+    }, {});
+    const action = createCart(newCart);
+    dispatch(action);
+  });
+}
 
 export const postToCart = (productId) => dispatch => {
   return axios
@@ -26,16 +70,42 @@ export const postToCart = (productId) => dispatch => {
     });
 };
 
-export const fetchCart = () => dispatch => {
+export const updateProductQuantity = (productId, newQuantity) => dispatch => {
   return axios
-  .get('/api/active-orders')
-  .then(res => res.data)
-
+    .put(`/api/active-orders/${productId}`, { newQuantity })
+    .then(res => res.data)
+    .then(updatedItem => {
+      const action = updateItemInCart(updatedItem);
+      dispatch(action);
+    });
 }
+
+export const deleteProductFromCart = productId => dispatch => {
+  return axios
+    .delete(`/api/active-orders/${productId}`)
+    .then(() => {
+      const action = deleteFromCart({ productId });
+      dispatch(action);
+    });
+};
+
+
+// reducer
 export default function cart(state = {}, action) {
   switch (action.type) {
+    case CREATE_CART:
+      return action.newCart;
     case ADD_TO_CART:
-      return Object.assign({}, state, action.order)
+      return Object.assign({}, state, action.order);
+    case UPDATE_ITEM_IN_CART:
+      return Object.assign({}, state, action.item);
+    case DELETE_FROM_CART:
+      return Object.keys(state).reduce((newCart, itemId) => {
+        if (+itemId !== +action.productId.productId) {
+          newCart[itemId] = state[itemId];
+        }
+        return newCart;
+      }, {});
     default:
       return state;
   }
