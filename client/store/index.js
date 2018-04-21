1dc1d6050a5109d0916099ddc7a64599622efaea
Da Woon Lee <daniel.lee9415@gmail.com>
2018-04-21 16:22:49 -0400

    cleaned up some code in store/index.js

--- client/store/index.js
+++ client/store/index.js
@@ -5,15 +5,9 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import user from './user';
 import products from './allproducts';
 import cart from './cart';
-<<<<<<< HEAD
 import pastOrders from './pastOrders';
 
 export const reducer = combineReducers({ user, products, cart, pastOrders });
-=======
-import sidebarReducer from './sidebar'
-
-export const reducer = combineReducers({ user, products, cart, sidebarReducer });
->>>>>>> master
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 );
