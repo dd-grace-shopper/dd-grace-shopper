9ed2564cc49f38285494656f08e9759d3f39332f
Jingjing Li <brelinlee@hotmail.com>
2018-04-22 22:40:55 -0400

    added cart ui and google distance matrix

--- client/components/ViewCart.js
+++ client/components/ViewCart.js
@@ -1,56 +1,100 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Table, Image, Button } from "semantic-ui-react";
 
 const ViewCart = props => {
   const cart = props.productsInCart ? props.productsInCart : null;
   const total = props.total;
   const numItems = props.numItems;
+  const destination = props.destination;
+  const finalDistance = props.finalDistance;
+
+  let shippingFee;
+  if (!finalDistance){
+    shippingFee = 0;
+  } else if (finalDistance < 50){
+      shippingFee = 5;
+  } else if (finalDistance <= 100) {
+      shippingFee = 8;
+  } else {
+      shippingFee = 10;
+  }
+
   return (
     <div>
-      <h1>Currently in Cart</h1>
-      <table >
-      <thead>
-        <tr>
-          <th>Price</th>
-          <th>Quantity</th>
-        </tr>
-        </thead>
-        <tbody>
-      {
-        cart.map(item => {
-          return (
-            <tr className="cartItem" key={item.productId}>
-              <td><img src={item.imageUrl} /></td>
-              <td>
-                {item.name}
-                <button
-                  onClick={evt => props.handleDelete(evt, item.productId)}
-                >Delete</button>
-              </td>{/*TODO: UI*/}
-              <td>{item.price}</td>
-              <td>{item.quantity}</td>
-              <td>
-                <button type="button" value="minus" onClick={evt => props.handleUpdate(evt,  item.productId, (item.quantity - 1))}>
-                  -
-                </button>
-                <button type="button" value="plus" onClick={evt => props.handleUpdate(evt,  item.productId, (item.quantity + 1))}>
-                  +
-                </button>
-              </td>
-            </tr>
-          );
-        })
-      }
-      </tbody>
+      <div className="ui left aligned container" >
+        <h1>Your Shopping Cart</h1>
+        <table className = "ui fixed table">
+        <thead>
+          <tr>
+            <th> </th>
+            <th className="4 wide">Product Name</th>
+            <th>Price</th>
+            <th className="2 wide">Quantity </th>
+            <th className="2 wide">Update </th>
+          </tr>
+          </thead>
+          <tbody>
+        {
+          cart.map(item => {
+            return (
+              <tr className="cartItem" key={item.productId}>
+                <td><img className ="ui small image" src={item.imageUrl} /></td>
+                <td>
+                  {item.name}
+                </td>{/*TODO: UI*/}
+                <td>{item.price}</td>
+                <td>{item.quantity}</td>
+                <td>
+                <button className = "ui secondary button"
+                onClick={evt => props.handleDelete(evt, item.productId)}
+                  >Delete</button>
+                  <button type="button" className = "ui button" value="minus" onClick={evt => props.handleUpdate(evt,  item.productId, (item.quantity - 1))}>
+                    -
+                  </button>
+                  <button type="button" className = "ui button" value="plus" onClick={evt => props.handleUpdate(evt,  item.productId, (item.quantity + 1))}>
+                    +
+                  </button>
+                </td>
+              </tr>
+            );
+          })
+        }
+        </tbody>
       </table>
-      <br />
-      <h4> {`Subtotal: (${numItems}items) : $ ${total}`} </h4>
+    </div>
+    <br />
 
-      <Link to={'/checkout'}>
-        <button>Checkout!</button>
+    <div className="ui right aligned container" >
+
+      <h4 > {`Subtotal: (${numItems} items) : $ ${total}`} </h4>
+
+      <form className="ui form">
+        <div className="field">
+          <label> Please type in city and state to estimate shipping cost</label>
+          <div className="ui fluid input">
+            <input type="text" placeholder="City, State" onChange = {props.handleChange} />
+            <button className="ui button" role="button"
+            onClick={evt => props.handleClickDistance(evt, destination)} >Submit</button>
+          </div>
+        </div>
+      </form>
+
+      <div >{!finalDistance ? 'Distance' : `Distance: ${finalDistance} miles`}</div>
+
+      <div >{
+        shippingFee ? `Shipping Fee: $ ${shippingFee}` : 'Shipping Cost'
+      }
+      </div>
+      <h4 > {`Total: $ ${total + shippingFee}`} </h4>
+      <Link  to={'/checkout'}>
+        <button className = "ui secondary button">Checkout!</button>
       </Link>
+
+      </div>
     </div>
   );
 }
 
 export default ViewCart;
+
--- client/store/index.js
+++ client/store/index.js
@@ -5,8 +5,9 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import user from './user';
 import products from './allproducts';
 import cart from './cart';
+import distance from './distance';
 
-export const reducer = combineReducers({ user, products, cart });
+export const reducer = combineReducers({ user, products, cart, distance });
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 );
