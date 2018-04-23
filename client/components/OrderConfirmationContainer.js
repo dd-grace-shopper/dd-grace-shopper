769eadb09ea01d9e9a42a0aaa07b0fb6fe07ec07
Ben Ellentuck <14841133+bellentuck@users.noreply.github.com>
2018-04-23 18:07:26 -0400

    Update OrderConfirmationContainer.js
    deleted console.log

--- client/components/OrderConfirmationContainer.js
+++ client/components/OrderConfirmationContainer.js
@@ -4,7 +4,6 @@ import { fetchOrder } from '../store/pastOrders';
 import OrderConfirmation from './OrderConfirmation';
 
 const mapState = function(state, ownProps) {
-  console.log('order confirm state', state);
   return {
     order: state.pastOrders,
     cart: state.cart
