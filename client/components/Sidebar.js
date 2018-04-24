b726e2b60da6dcb9906fe71bf89737b40fe1947c
Ben Ellentuck <ellentuckben@gmail.com>
2018-04-24 12:34:22 -0400

    sidebar filter reset button working, sidebar and sidebarContainer refactored

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -3,31 +3,54 @@ import { Sidebar, Segment, Button, Menu, Image, Icon, Header, Form, Checkbox,Lab
 
 export default class SidebarLeft extends Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {}; // hash of what categories are currently checked
+    this.toggleSingleFilterCheckbox = this.toggleSingleFilterCheckbox.bind(this);
+    this.resetAllFilterBoxes = this.resetAllFilterBoxes.bind(this);
   }
 
   componentDidMount() {
-    this.props.getAllProducts ? this.props.getAllProducts() : null;
+    if (this.props.getAllProducts) this.props.getAllProducts();
+    if (this.props.uniqueCategoriesForSidebar) {
+      this.props.uniqueCategoriesForSidebar.forEach(category => {
+        this.setState({ [category]: false });
+      });
+    }
+    if (this.props.uniqueSizesForSidebar) {
+      this.props.uniqueSizesForSidebar.forEach(size => {
+        this.setState({ [size]: false });
+      });
+    }
+    if (this.props.uniquePricesForSidebar) {
+      this.props.uniquePricesForSidebar.forEach(price => {
+        this.setState({ [price]: false });
+      });
+    }
+  }
+
+  componentWillUnmount() {
+    this.props.resetSidebarFilters();
+  }
+
+  toggleSingleFilterCheckbox(filterName) {
+    this.setState(prevState => {
+      return { [filterName]: !prevState[filterName] };
+    });
+  }
+  resetAllFilterBoxes() {
+    this.setState(prevState => {
+      return Object.keys(prevState).reduce((newState, filterName) => {
+        newState[filterName] = false;
+        return newState;
+      }, {});
+    });
   }
 
   render() {
-    let { visible, products, productsById, filter } = this.props ? this.props : null;
-
-      // Getting unique product categories, sizes, and prices to display in sidebar filter
-      const CategoriesForSidebar = products.length && products.map(id => {
-        return productsById[id].category.category
-      })
-      const uniqueCategoriesForSidebar = Array.from(new Set(CategoriesForSidebar));
-
-      const SizesForSidebar = products.length && products.map(id => {
-        return productsById[id].size.mililiter
-      })
-      const uniqueSizesForSidebar = Array.from(new Set(SizesForSidebar)).sort();
-
-      const pricesForSidebar = products.length && products.map(id => {
-        return productsById[id].priceRange
-      })
-      const uniquePricesForSidebar = Array.from(new Set(pricesForSidebar)).sort();
+    let {
+      visible, products, productsById, filter,
+      uniqueCategoriesForSidebar, uniqueSizesForSidebar, uniquePricesForSidebar
+    } = this.props ? this.props : null;
 
       return (
         <div>
@@ -36,28 +59,72 @@ export default class SidebarLeft extends Component {
               <label>Category</label>
                 {
                   products && uniqueCategoriesForSidebar.map(category => {
-                    return <div key={category}>
-                      <Form.Field label={category} value={category} control='input' type='checkbox' onChange={this.props.handleChange} />
-                    </div>
+                    return (
+                      <div key={category}>
+                        <Form.Field
+                          label={category}
+                          value={category}
+                          control="input"
+                          type="checkbox"
+                          onChange={event => {
+                            this.toggleSingleFilterCheckbox(category);
+                            this.props.handleChange(event);
+                          }}
+                          checked={this.state[category]}
+                        />
+                      </div>
+                    );
                   })
                 }
               <label>Size</label>
                 {
                   products && uniqueSizesForSidebar.map(size => {
-                    return <div key={size}>
-                      <Form.Field label={size} value={size} control='input' type='checkbox' onChange={this.props.handleChange} />
-                    </div>
+                    return (
+                      <div key={size}>
+                        <Form.Field
+                          label={size}
+                          value={size}
+                          control="input"
+                          type="checkbox"
+                          onChange={event => {
+                            this.props.handleChange(event);
+                            this.toggleSingleFilterCheckbox(size);
+                          }}
+                          checked={this.state[size]}
+                        />
+                      </div>
+                    );
                   })
                 }
               <label>Price</label>
                 {
                   products && uniquePricesForSidebar.map(price => {
-                    return <div key={price}>
-                      <Form.Field label={price} value={price} control='input' type='checkbox' onChange={this.props.handleChange} />
-                    </div>
+                    return (
+                      <div key={price}>
+                        <Form.Field
+                          label={price}
+                          value={price}
+                          control="input"
+                          type="checkbox"
+                          onChange={event => {
+                            this.toggleSingleFilterCheckbox(price);
+                            this.props.handleChange(event);
+                          }}
+                          checked={this.state[price]}
+                        />
+                      </div>
+                    );
                   })
                 }
-              <Button type="reset" onClick={this.props.handleClick} >Reset</Button>
+              <Button
+                type="reset"
+                onClick={event => {
+                  this.props.handleClick(event);
+                  this.resetAllFilterBoxes();
+                }}
+              >
+                Reset
+              </Button>
             </Form.Group>
           </Sidebar>
 
