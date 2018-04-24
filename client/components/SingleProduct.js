8531288d8df7b12ac6a47c56191e9d2b0a89e046
Yoni Slotwinere <yonislots@gmail.com>
2018-04-23 20:05:25 -0400

    misc

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -24,27 +24,29 @@ export default class SingleProduct extends Component {
               {product.imageUrl && (
                 <img className="single-product-img" src={product.imageUrl} />
               )}
-              <h4 className="single-product-description">
-                Description: {product.description}
-              </h4>
-              {product.category && (
-                <h4 className="single-product-category">
-                  Category: {product.category.category}
+              <div className="single-product-description-section">
+                <h4 className="single-product-description">
+                  Description: {product.description}
                 </h4>
-              )}
-              {product.country && (
-                <h4 >
-                  Country: {product.country.name}
+                {product.category && (
+                  <h4 className="single-product-category">
+                    Category: {product.category.category}
+                  </h4>
+                )}
+                {product.country && (
+                  <h4 className="single-product-country">
+                    Country: {product.country.name}
+                  </h4>
+                )}
+                {product.size && (
+                  <h4 className="single-product-size">
+                    Size: {product.size.mililiter}
+                  </h4>
+                )}
+                <h4 className="single-product-price">
+                  Price: {product.price}
                 </h4>
-              )}
-              {product.size && (
-                <h4 className="single-product-size">
-                  Size: {product.size.mililiter}
-                </h4>
-              )}
-              <h4 className="single-product-price">
-                Price: {product.price}
-              </h4>
+              </div>
             </div>
           </div>
         )}
